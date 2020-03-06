72ed50bb5789160b317e91d565d6949e2ac13fd1
Dupe Fadina <maafadina@hotmail.com>
2020-03-06 13:45:42 -0800

    Added functions to view product sales and create new departments.

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -48,7 +48,38 @@ function getSupervisorAction() {
     });
 }
 
-function viewProductSales() {}
+function viewProductSales() {
+  var inventory = [];
+  var targetColumns = [
+    "d.department_id",
+    "d.deparment_name",
+    "d.over_head_costs",
+    "p.product_sales",
+    "p.product_sales - d.over_head_costs",
+    "total_profits",
+    "products",
+    "p",
+    "departments",
+    "p",
+    "p.department_name",
+    "d.deparment_name"
+  ];
+  connection.query(
+    "SELECT ??, ??, ??, ??, ?? AS ??  FROM ?? AS ?? RIGHT JOIN ?? AS ?? ON ?? = ??",
+    targetColumns,
+    function(error, results, fields) {
+      // error will be an Error if one occurred during the query
+      // results will contain the results of the query
+      // fields will contain information about the returned results fields (if any)
+      //populate inventory with store items.
+      for (var item of results) {
+        inventory.push(item);
+      }
+      var prettyTable = cTable.getTable(inventory);
+      console.log(prettyTable);
+    }
+  );
+}
 
 function createDepartment() {
   //prompts supervisor for an action..
